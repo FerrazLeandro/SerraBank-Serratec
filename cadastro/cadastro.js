1ff11c1eef1e6d60088f2f42eed54e601cd3bef4
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-09-29 19:04:54 -0300

    Melhorias e usabilidade

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const limparFormulario = (endereco) =>{
+const limparFormulario = (endereco) => {
     document.getElementById("inputNome").value = '';
     document.getElementById("inputCpf").value = '';
     document.getElementById("inputEmail").value = '';
@@ -14,7 +14,7 @@ const limparFormulario = (endereco) =>{
 }
 
 
-const preencherFormulario = (endereco) =>{
+const preencherFormulario = (endereco) => {
     document.getElementById('endereco').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('cidade').value = endereco.localidade;
@@ -24,47 +24,34 @@ const preencherFormulario = (endereco) =>{
 
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 
-const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
-const pesquisarCep = async() => {
+const pesquisarCep = async () => {
     const cep = document.getElementById('cep').value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    if (cepValido(cep)){
+    if (cepValido(cep)) {
         const dados = await fetch(url);
         const endereco = await dados.json();
-        if (endereco.hasOwnProperty('erro')){
+        if (endereco.hasOwnProperty('erro')) {
             document.getElementById('endereco').value = 'CEP não encontrado!';
-        }else {
+        } else {
             preencherFormulario(endereco);
         }
-    }else{
+    } else {
         document.getElementById('endereco').value = 'CEP incorreto!';
     }
-     
+
 }
 
 document.getElementById('cep')
-        .addEventListener('focusout', pesquisarCep);
+    .addEventListener('focusout', pesquisarCep);
 
 
 
 document.getElementById("cadastrar").onclick = cadastrar;
 
 
-async function cadastrar ()  {
-
-    var main = document.getElementById("load");
-
-       main.innerHTML += `
-        <div class="load">
-            <div class="d-flex justify-content-center">
-                <div class="spinner-border" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
-        </div>
-        `;
-
+async function cadastrar() {
     var nome = document.getElementById("inputNome").value;
     var cpf = document.getElementById("inputCpf").value;
     var email = document.getElementById("inputEmail").value;
@@ -75,8 +62,25 @@ async function cadastrar ()  {
     var bairro = document.getElementById("bairro").value;
     var cidade = document.getElementById("cidade").value;
     var estado = document.getElementById("estado").value;
-
-    let data =  {
+    
+    
+    if (nome === '' || cpf === ''  || cpf.length <= 11) {
+        alert('CPF e/ou nome inválido(s)')
+    } else {
+        
+        var main = document.getElementById("load");
+    
+        main.innerHTML += `
+            <div class="load">
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
+            `;
+        
+    let data = {
         "nome": nome,
         "cpf": cpf,
         "email": email,
@@ -90,24 +94,25 @@ async function cadastrar ()  {
         "status": "Em análise",
     };
 
-   fetch("https://6333633c433198e79dc444a9.mockapi.io/cadastro", {
-     method: "POST",
-     headers: {
-       "Content-Type": "application/json",
-     },
-     body: JSON.stringify(data),
-   })
-     .then((response) => response.json())
-     .then((data) => {
-       console.log("Success:", data);
-       localStorage.setItem("cpf", JSON.stringify(cpf))
-       main.remove(); 
-       limparFormulario()
-       alert("Cadastro realizado com sucesso, você será redirecionado para consultar seu cadastro :)");
-       window.location.href = "../consulta/consulta.html";
-     })
-     .catch((error) => {
-        alert("Erro ao cadastrar usuário :(");
-     });
+    fetch("https://6333633c433198e79dc444a9.mockapi.io/cadastro", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+            localStorage.setItem("cpf", JSON.stringify(cpf))
+            main.remove();
+            limparFormulario()
+            alert("Cadastro realizado com sucesso, você será redirecionado para consultar seu cadastro :)");
+            window.location.href = "../consulta/consulta.html";
+        })
+        .catch((error) => {
+            alert("Erro ao cadastrar usuário :(");
+        });
+    }
 
- }
+}
--- consulta/consulta.js
+++ consulta/consulta.js
@@ -3,7 +3,7 @@ var cpf = JSON.parse(json);
 document.getElementById("inputCpf").value = cpf;
 
 
-const limparFormulario = () =>{
+const limparFormulario = () => {
     document.getElementById("inputNome").value = '';
     document.getElementById("inputCpf").value = '';
     document.getElementById("inputEmail").value = '';
@@ -15,22 +15,26 @@ const limparFormulario = () =>{
 
 document.getElementById("consultar").onclick = pesquisar;
 
-async function pesquisar ()  {
+async function pesquisar() {
     var cpf = document.getElementById("inputCpf").value;
-    var repo = await fetch("https://6333633c433198e79dc444a9.mockapi.io/cadastro/");
-    var pessoas = await repo.json();
-    var pessoa
 
-    if (pessoa = pessoas.find((pessoas) => pessoas.cpf === cpf)) {
-        document.getElementById("inputNome").value = pessoa.nome;
-        document.getElementById("inputTelefone").value = pessoa.telefone;
-        document.getElementById("inputEmail").value = pessoa.email;
-        document.getElementById("inputStatus").value = pessoa.status;
+    if (cpf === '' || cpf.length <= 11) {
+        alert('CPF inválido')
     } else {
-        alert("CPF " + cpf + " não encontrado :(");
-        limparFormulario();
+        var repo = await fetch("https://6333633c433198e79dc444a9.mockapi.io/cadastro/");
+        var pessoas = await repo.json();
+        var pessoa
+
+        if (pessoa = pessoas.find((pessoas) => pessoas.cpf === cpf)) {
+            document.getElementById("inputNome").value = pessoa.nome;
+            document.getElementById("inputTelefone").value = pessoa.telefone;
+            document.getElementById("inputEmail").value = pessoa.email;
+            document.getElementById("inputStatus").value = pessoa.status;
+        } else {
+            alert("CPF " + cpf + " não encontrado :(");
+            limparFormulario();
+        }
     }
 
 
 }
-
