a48f0a2fc833691eb0b1dba45acbf72178005872
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-09-30 16:24:02 -0300

    Melhorias e usabilidade

--- pop-up/js/editar.js
+++ pop-up/js/editar.js
@@ -1,13 +1,13 @@
       bioEp.init({
             html: '<div id="bio_ep_content">' +
             
-            '<span></span>' +
+            '<p>Conheça agora mesmo e economize dinheiro!</p>' +
             '<span></span>' +
             '<span></span>' +
             '<a href="../cadastro/cadastro.html" class="bio_btn">Clique e se cadastre &nbsp; </i></a>' +
             '</div>',
-            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(../../img/SerraBank.png);background-size: cover;}' +
-            '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
+            css: '#bio_ep {width: 400px; height: 200px; color: #FFF; background: linear-gradient(black, #ff4d7b 70%, black ); text-align: center;background-size: cover; border-radius: 3%;}' +
+            '#bio_ep_content {padding: 50px 0 0 0; font-family: inherit;}' +
             '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
             '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
             '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
